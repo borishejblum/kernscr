4099c3f26039c4bebba616a150d525f6e6cb6f43
borishejblum <bhehblum.hsph@gmail.com>
2016-04-22 16:17:28 -0400

    exporting handy VTM function

--- R/VTM.R
+++ R/VTM.R
@@ -1,13 +1,14 @@
-#' VTM
+#'VTM
 #'
-#' Repeats a vector as identical rows of a matrix
+#'Repeats a vector as identical rows of a matrix
 #'
-#' @param vc vector to be repeated
-#' @param dm number of times that \code{vc} should be repeated
+#'@param vc vector to be repeated
+#'@param dm number of times that \code{vc} should be repeated
 #'
-#' @return a matrix of \code{dm} rows
+#'@return a matrix of \code{dm} rows
 #'
-#' @keywords internal
+#'@keywords internal
+#'@export
 VTM <- function(vc, dm){
   matrix(vc, ncol=length(vc), nrow=dm, byrow=T)
 }
