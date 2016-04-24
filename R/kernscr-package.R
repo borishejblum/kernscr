c4c09a2114351305ec45e3bed98f72209557567b
borishejblum <bhejblum.hsph@gmail.com>
2016-04-24 18:44:45 -0400

    ready for CRAN submission

--- R/kernscr-package.R
+++ R/kernscr-package.R
@@ -1,15 +1,25 @@
-#' kernscr: a package to perform Kernel Machine Score Test for Pathway Analysis in the Presence of Semi-Competing Risks
+#'kernscr: a package to perform Kernel Machine Score Test for Pathway Analysis in the Presence of Semi-Competing Risks
 #'
-#' Kernel Machine Score Test for Pathway Analysis in the Presence of Semi-Competing Risks
+#'Kernel Machine Score Test for Pathway Analysis in the Presence of Semi-Competing Risks
 #'
-#' The main functions of the kernscr package is \code{\link{compute_all_tests}}
 #'
-#' @docType package
+#'\tabular{ll}{
+#'Package: \tab kernscr\cr
+#'Type: \tab Package\cr
+#'Version: \tab 1.0.0\cr
+#'Date: \tab 2016-04-28\cr
+#'License:\tab \href{http://www.gnu.org/licenses/gpl-2.0.txt}{GPL-2}\cr
+#'}
+#'The main function of the kernscr package is \code{\link{compute_all_tests}}
 #'
-#' @references Neykov M, Hejblum BP, Sinnot JA,  Kernel Machine Score Test for
-#' Pathway Analysis in the Presence of Semi-Competing Risks, \emph{submitted}, 2016.
+#'@author Boris P. Hejblum, Matey Neykov, Jennifer A. Sinnott
+#'--- Maintainer: Boris P. Hejblum
 #'
-#' @name kernscr-package
-#' @aliases kernscr
+#'@references Neykov M, Hejblum BP, Sinnot JA,  Kernel Machine Score Test for
+#'Pathway Analysis in the Presence of Semi-Competing Risks, \emph{submitted}, 2016.
+#'
+#'@docType package
+#'@name kernscr-package
+#'@aliases kernscr
 #'
 NULL
